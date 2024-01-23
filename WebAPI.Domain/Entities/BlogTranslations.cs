372d87f9e0a1fe5d03a04265b5a96272ba158401
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-01-23 17:53:19 +0300

    23-01-2024-17-53

---
+++ WebAPI.Domain/Entities/BlogTranslations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Domain.Entities.Common;
+
+namespace WebAPI.Domain.Entities
+{
+    public class BlogTranslations:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Brief { get; set; }
+        public string MetaDescription { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public virtual ICollection< MyProperty { get; set; }
+    }
+}
