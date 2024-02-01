8c54c4b21c1f22a6312dad5ecbc991b7356fd9b0
ibrahim Girişken <ibrahimgirisken@hotmail.com>
2024-02-01 17:06:36 +0300

    01-02-2024-17-06

--- WebAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
+++ WebAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI.Application.Abstractions.Services;
 using WebAPI.Application.Abstractions.Token;
 using WebAPI.Application.DTOs;
 using WebAPI.Application.Exceptions;
@@ -13,39 +14,20 @@ namespace WebAPI.Application.Features.Commands.AppUser.LoginUser
 {
     public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
     {
-        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
-        readonly SignInManager<Domain.Entities.Identity.AppUser> _signInManager;
-        readonly ITokenHandler _tokenHandler;
+        readonly IAuthService _authService;
 
-        public LoginUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, SignInManager<Domain.Entities.Identity.AppUser> signInManager, ITokenHandler tokenHandler = null)
+        public LoginUserCommandHandler(IAuthService authService)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _tokenHandler = tokenHandler;
+            _authService = authService;
         }
 
         public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
         {
-            Domain.Entities.Identity.AppUser user = await _userManager.FindByNameAsync(request.UserNameOrEmail);
-            if (user == null)
-                user = await _userManager.FindByEmailAsync(request.UserNameOrEmail);
-
-            if (user == null)
-                throw new NotFoundUserException();
-
-            SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
-            if (result.Succeeded)//Authentication başarılı !
+            var token = await _authService.LoginAsync(request.UserNameOrEmail, request.Password, 900);
+            return new LoginUserSuccessCommandResponse()
             {
-                //yetkiler belirlenecek!
-                Token token = _tokenHandler.CreateAccessToken(5);
-                return new LoginUserSuccessCommandResponse()
-                {
-                    Token = token
-                };
-            }
-             throw new AuthenticationErrorException();
-            
-
+                Token = token
+            };
         }
     }
 }
